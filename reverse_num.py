a64cd466148591939f77e3bae19ac3ff361ae76e
Suleiman Ibrahim <suleimanonnet@gmail.com>
2020-07-12 14:04:16 +0100

    Master piece of its type

---
+++ reverse_num.py
@@ -0,0 +1,31 @@
+def two(num):
+    num0 = int(num)
+    num2 = num0 % 10
+    num1 = int(num0 / 10)
+    print(f'The reversed form of {num} is {num2}{num1}')
+
+def three(num):
+    num0 = int(num)
+    num3 = num0 % 10
+    num2 = int((num0 % 100) / 10)
+    num1 = int(num0 / 100)
+    print(f'The reversed form of {num0} is {num3}{num2}{num1}')
+
+def four(num):
+    num0 = int(num)
+    num4 = num0 % 10
+    num3 = int((num0 % 100) / 10)
+    num2 = int((num0  % 1000) / 100)
+    num1 = int(num0 / 1000)
+    print(f'The reversed form of {num0} is {num4}{num3}{num2}{num1}')
+
+while True: 
+    num = input("Enter the number to be reversed: ")
+    if len(num) == 2:
+        two(num)
+    if len(num) == 3:
+        three(num)
+    if len(num) == 4:
+        four(num)
+    if len(num) == 5:
+        five(num)
